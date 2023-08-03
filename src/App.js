4dc084ff8b6d2a3e58b389041d53e249a5de4cfc
aastha-cse <guptaaastha824@gmail.com>
2023-08-03 18:20:03 +0530

    mychanges

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import { TodoWrapper } from './components/TodoWrapper';
 function App() {
   return (
     <div className="App">
+      <div>
+      <h1 id="intro">
+        <span>SUCCESS &nbsp;</span>
+        <span>IS &nbsp;</span>
+        <span>NOT &nbsp;</span>
+        <span>FOR &nbsp;</span>
+        <span>THE &nbsp;</span>
+        <span>LAZY</span>
+      </h1>
+      </div>
       <TodoWrapper/>
     </div>
   );
